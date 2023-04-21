d5a546c456d8e2ac9792e72b9171e95ce2fd7c7b
Abrahanstd <105933092+Abrahanstd@users.noreply.github.com>
2023-04-21 14:43:15 -0600

    Update load.js

--- load.js
+++ load.js
@@ -1,8 +1,1 @@
-var newDiv = document.createElement("div");
-  var newContent = document.createTextNode("Hola!¿Qué tal?");
-  newDiv.appendChild(newContent); //añade texto al div creado.
-
-  // añade el elemento creado y su contenido al DOM
-  var currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
-alert("xD");
+Console.log("asdasdasd")
